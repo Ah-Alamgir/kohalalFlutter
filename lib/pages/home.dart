dbbc135fc6cc740407f07e0fc69df71f04123f68
blackmonk13 <spencervalentino13@gmail.com>
2024-05-05 08:26:12 +0300

    use error view in hompage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,7 @@ import 'package:beamer/beamer.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:text_scanner/components/common/error_view.dart';
 import 'package:text_scanner/providers/providers.dart';
 
 import 'package:text_scanner/components/common/join_btns.dart';
@@ -139,113 +140,51 @@ class HomePage extends HookConsumerWidget {
             );
           },
           error: (error, stackTrace) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-              ),
-              child: Column(
-                children: [
-                  Card(
-                    elevation: .5,
-                    child: InkWell(
-                      onTap: () async {
-                        await ref
-                            .read(imagePickerControllerProvider.notifier)
-                            .pickImage();
-                      },
-                      child: SizedBox(
-                        height: MediaQuery.of(context).size.height * .7,
-                        width: MediaQuery.of(context).size.width,
-                        child: SingleChildScrollView(
-                          primary: false,
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 16.0,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                FluentIcons.warning_20_regular,
-                                size: MediaQuery.of(context).size.shortestSide *
-                                    .15,
-                              ),
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: 'Oops, something went wrong.\n\n',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineSmall,
-                                    ),
-                                    const TextSpan(
-                                      children: [TextSpan(text: "\n\n")],
-                                    ),
-                                    TextSpan(
-                                      children: [
-                                        TextSpan(
-                                          text: 'Details\n',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .titleMedium,
-                                        ),
-                                        TextSpan(
-                                          text: "${error.toString()}\n\n",
-                                        ),
-                                      ],
-                                    ),
-                                    TextSpan(
-                                      children: [
-                                        TextSpan(
-                                          text: "Solution\n",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .titleMedium,
-                                        ),
-                                        const TextSpan(
-                                          text:
-                                              "Please pick a different image.\n\n",
-                                        ),
-                                      ],
-                                    ),
-                                    const TextSpan(
-                                      text:
-                                          "If you need further assistance, please reach out to our support team.",
-                                    ),
-                                  ],
-                                ),
-                                textAlign: TextAlign.center,
-                              )
-                            ],
-                          ),
-                        ),
+            return Column(
+              children: [
+                Card(
+                  elevation: .5,
+                  child: InkWell(
+                    onTap: () async {
+                      await ref
+                          .read(imagePickerControllerProvider.notifier)
+                          .pickImage();
+                    },
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * .7,
+                      width: MediaQuery.of(context).size.width,
+                      child: AsyncErrorView(
+                        error: error,
+                        stackTrace: stackTrace,
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 20.0,
-                    ),
-                    child: JoinBtns(
-                      buttons: [
-                        JoinBtn(
-                          onPressed: () async {
-                            await ref
-                                .read(imagePickerControllerProvider.notifier)
-                                .pickImage();
-                          },
-                          // icon: FluentIcons.image_add_24_regular,
-                          label: "Pick Image",
-                        ),
-                      ],
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20.0,
                   ),
-                ],
-              ),
+                  child: JoinBtns(
+                    buttons: [
+                      JoinBtn(
+                        onPressed: () async {
+                          await ref
+                              .read(imagePickerControllerProvider.notifier)
+                              .pickImage();
+                        },
+                        // icon: FluentIcons.image_add_24_regular,
+                        label: "Pick Image",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             );
           },
           loading: () {
-            return const Center(child: CircularProgressIndicator());
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           },
         ),
       ),
