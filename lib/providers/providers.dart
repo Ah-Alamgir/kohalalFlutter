d4e46c6c5b833cd1ec0653cda58f1d3cdd8ec133
blackmonk13 <spencervalentino13@gmail.com>
2025-01-28 03:48:44 +0300

    edit history, revert to original, undo and redo for image cropping and rotation

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -3,6 +3,7 @@ library providers;
 import 'dart:io';
 import 'dart:ui' as ui;
 
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:path/path.dart' as p;
 
